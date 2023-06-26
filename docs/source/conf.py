9368c8e53d6ff92eee3d4015163e90c0baf8f91f
mVenetos97 <53448901+mVenetos97@users.noreply.github.com>
2023-06-26 14:41:31 -0700

    mock_imports

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,6 +24,7 @@ author = 'Maxwell Venetos'
 # The full version, including alpha/beta/rc tags
 release = '0.0.1'
 
+autodoc_mock_imports = ['numpy', 'eigenn', 'e3nn', 'torch']
 
 # -- General configuration ---------------------------------------------------
 
